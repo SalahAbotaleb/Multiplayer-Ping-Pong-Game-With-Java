829d61008456675a01a90565a5ecd3f71056a4da
salah <salah.m.abotaleb@gmail.com>
2023-06-17 14:35:43 +0300

    writing initial tasks of the game in PongGame.java

--- src/PongGame.java
+++ src/PongGame.java
@@ -4,6 +4,15 @@ import javax.swing.*;
 public class PongGame {
     public static void main(String[] args)
     {
+        /*PongGame instantiates GameFrame which contains only basic
+        * configurations of the Frame while GameFrame instantiates GamePanel
+        * which is responsible for the following:
+        * 1. adding a new thread
+        * 2. initialize the panel size
+        * 3. have the run method that infinitely runs till the close of the app
+        * 4. instantiate Paddel, Ball and Score objects
+        * 5. call appropriate functions from Paddel, Ball and Score objects upon occurrence of specific events
+        *  */
         new GameFrame();
     }
 
